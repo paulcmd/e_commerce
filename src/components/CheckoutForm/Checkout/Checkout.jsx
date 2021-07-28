09de4496786ac14507c01ef8395ac6cb6c7860e2
Paul <choxmail.pm@gmail.com>
2021-07-27 20:42:11 -0700

    added Address component

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -10,12 +10,22 @@ import {
     Button
 } from '@material-ui/core'
 import useStyles from './styles'
+import AddressForm from '../AddressForm'
+import PaymentForm from '../PaymentForm'
+
 
 const steps = ['Shipping address', 'Payment details']
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0)
-    const classes = useStyles()
+	const classes = useStyles()
+	
+	const Confirmation = () => (
+		<div>
+			Confirmation
+	</div>
+		)
+
     const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />)
 
     return (
@@ -36,7 +46,7 @@ const Checkout = () => {
                             </Step>
                         ))}
 					</Stepper>
-					{activeStep === steps.lastIndex ? <Confirmation /> : <From />}
+					{activeStep === steps.lastIndex ? <Confirmation /> : <Form />}
                 </Paper>
             </main>
         </>
