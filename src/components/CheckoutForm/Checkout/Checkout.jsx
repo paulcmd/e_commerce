a73acbc4fc3e994f5b0d424368f73d4aa156c568
Paul <choxmail.pm@gmail.com>
2021-07-27 19:57:20 -0700

    added checkout stepper

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    Paper,
+    Stepper,
+    Step,
+    StepLabel,
+    Typography,
+    CircularProgress,
+    Divider,
+    Button
+} from '@material-ui/core'
+import useStyles from './styles'
+
+const steps = ['Shipping address', 'Payment details']
 
 const Checkout = () => {
-	return (
-		<div>
-			
-		</div>
-	)
+	const [activeStep, setActiveStep] = useState(0)
+	const classes = useStyles()
+
+    return (
+        <>
+            <div className={classes.toolbar} />
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                    <Typography variant="h4" align="center">
+                        Checkout
+                    </Typography>
+                    <Stepper
+                        activeStep={activeStep}
+                        className={classes.stepper}
+                    >
+                        {steps.map((step, index) => (
+                            <Step key={index}>
+                                <StepLabel>{step}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                </Paper>
+            </main>
+        </>
+    )
 }
 
 export default Checkout
