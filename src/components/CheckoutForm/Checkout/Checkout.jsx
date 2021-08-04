2ca4895301c4ca6e3fa35c40163e3498abef204f
Paul <choxmail.pm@gmail.com>
2021-08-04 16:27:26 -0700

    setup stripe public key

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -55,7 +55,7 @@ const Checkout = ({ cart }) => {
         activeStep === 0 ? (
             <AddressForm checkoutToken={checkoutToken} next={next}/>
         ) : (
-            <PaymentForm checkoutToken={checkoutToken}/>
+            <PaymentForm checkoutToken={checkoutToken} backStep={backStep}/>
         )
 
     return (
--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,23 +1,89 @@
 import React from 'react'
 import { Typography, Button, Divider } from '@material-ui/core'
-import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js'
-import { loadStrip } from '@stripe/stripe-js'
+import {
+    Elements,
+    CardElement,
+    ElementsConsumer
+} from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 
 import Review from './Review'
 
-const PaymentForm = ({ checkoutToken }) => {
-	return (
-		<>
-			<Review checkoutToken={checkoutToken} />
-			<Divider />
-			<Typography variant='h6' gutterBottom style={{ margin: '20px 0' }}>
-				Payment Method
-			</Typography>
-			<Elements>
-				
-			</Elements>
-		</>
-	)
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+
+const PaymentForm = ({ checkoutToken, backStep }) => {
+    const handleSubmit = (event, elements, stripe) => {
+        event.preventDefault()
+        if (!stripe || !elements) {
+            return
+        }
+        const cardElement = elements.getElement(CardElement)
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card: cardElement
+		})
+
+		if (error) {
+			console.log('Payment Method Creation Error : ', error)
+		}
+		
+
+    }
+    return (
+        <>
+            <Review checkoutToken={checkoutToken} />
+            <Divider />
+            <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>
+                Payment Method
+            </Typography>
+            <Elements stripe={stripePromise}>
+                <ElementsConsumer>
+                    {({ elements, stripe }) => (
+                        <form
+                            onSubmit={(event) =>
+                                handleSubmit(event, elements, stripe)
+                            }
+                        >
+                            <CardElement />
+                            <br /> <br />
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'space-between'
+                                }}
+                            >
+                                <Button
+                                    variant="outlined"
+                                    onClick={() => backStep()}
+                                >
+                                    Back
+                                </Button>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    disabled={!stripe}
+                                    color="primary"
+                                >
+                                    Pay{' '}
+                                    {
+                                        checkoutToken.live.subtotal
+                                            .formatted_with_symbol
+                                    }
+                                </Button>
+                            </div>
+                        </form>
+                    )}
+                </ElementsConsumer>
+            </Elements>
+        </>
+    )
 }
 
 export default PaymentForm
+
+/* we are passing the event (click event) to the form thru handleSubmit. the other 2 stripe dependencies
+are needed to create the final object
+e.preventDefault will stop page from refreshing( button doing default action in a form)
+
+*/
